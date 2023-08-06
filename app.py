044db80b1c77afa23e9c75fef6776215940d030e
Shea Leslein <sleslein@users.noreply.github.com>
2023-08-06 09:33:35 -0500

    added edit feature

--- app.py
+++ app.py
@@ -24,6 +24,21 @@ def contacts_view(contact_id=0):
 	contact = Contact.find(contact_id)
 	return render_template('show.html', contact=contact)
 
+@app.route("/contacts/<contact_id>/edit", methods=['GET'])
+def contacts_edit_get(contact_id=0):
+	contact = Contact.find(contact_id)
+	return render_template('edit.html', contact=contact)
+
+@app.route("/contacts/<contact_id>/edit", methods=['POST'])
+def contacts_edit_post(contact_id=0):
+	c = Contact.find(contact_id)
+	c.update(request.form['first_name'], request.form['last_name'], request.form['phone'], request.form['email'])
+	if c.save():
+		flash("Updated Contact!")
+		return redirect("/contacts/" + str(contact_id))
+	else:
+		return render_template('edit.html', contact=c)
+
 @app.route("/contacts/new", methods=['GET'])
 def contacts_new_get():
 		return render_template('new.html', contact=Contact())
