21a510a4bc6863222faf6e91d1d085982017a604
Shea Leslein <sleslein@users.noreply.github.com>
2023-08-12 11:26:31 -0500

    added htmx delete

--- app.py
+++ app.py
@@ -24,6 +24,13 @@ def contacts_view(contact_id=0):
 	contact = Contact.find(contact_id)
 	return render_template('show.html', contact=contact)
 
+@app.route("/contacts/<contact_id>", methods=['DELETE'])
+def contacts_delete(contact_id=0):
+	contact = Contact.find(contact_id)
+	contact.delete()
+	flash('Deleted Contact!')
+	return redirect("/contacts", 303)
+
 @app.route("/contacts/<contact_id>/edit", methods=['GET'])
 def contacts_edit_get(contact_id=0):
 	contact = Contact.find(contact_id)
@@ -39,12 +46,6 @@ def contacts_edit_post(contact_id=0):
 	else:
 		return render_template('edit.html', contact=c)
 
-@app.route("/contacts/<contact_id>/delete", methods=['POST'])
-def contacts_delete(contact_id=0):
-	contact = Contact.find(contact_id)
-	contact.delete()
-	flash('Deleted Contact!')
-	return redirect("/contacts")
 
 @app.route("/contacts/new", methods=['GET'])
 def contacts_new_get():
